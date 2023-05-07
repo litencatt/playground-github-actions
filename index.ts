5fb4a4b5bf7d53e1e592f5c3d59fe1aeed3621fe
litencatt <ncl0709@gmail.com>
2023-05-07 13:41:29 +0900

    Generate payload.json with TS

---
+++ index.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs'
+
+const payload = {
+  "blocks": [
+    {
+      "type": "header",
+      "text": {
+        "type": "plain_text",
+        "text": "タグ付け未完了エスカレタスク一覧",
+        "emoji": true
+      }
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "plain_text",
+        "text": "以下タグ付けされていないためタグ付けをお願いします:pray:"
+      }
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "- <https://google.com|Page1>\n- <https://google.com|Page2>\n- <https://google.com|Page3>"
+      }
+    }
+  ]
+}
+console.dir(payload, { depth: null})
+fs.writeFileSync("payload.json", payload, { encoding: 'utf-8' })
